a6ac4d6ff798fb1159f1910583f18f83c0486070
EunhakLee <admin@return0927.xyz>
2019-07-27 21:55:34 +0900

    Updated to load setting from settings.json

--- run.py
+++ run.py
@@ -1,9 +1,19 @@
 import logging
+from json import load
 from CfTwMonitor import Monitor
 
 logging.basicConfig(level=logging.INFO)
 
+settings = load(open("settings.json"))
 
+
+mon = Monitor(
+    token=settings['credentials']['discord']['bot'],
+    host="0.0.0.0",
+    port=80,
+    debug=True,
+    prefix="에낙아 "
+)
 
 mon.setup()
 mon.sanic.load_routes(
@@ -13,7 +23,6 @@ mon.sanic.load_routes(
     ]
 )
 print(" Sanic routes ", mon.sanic.router)
-
 
 mon.discord.load_extension("cogs.logger")
 mon.discord.load_extension("cogs.helper")
