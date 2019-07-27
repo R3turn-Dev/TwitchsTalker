65aae64315d7f077ad34fdadce429061bc326ea1
EunhakLee <admin@return0927.xyz>
2019-07-27 21:55:55 +0900

    Added alias to help command

--- cogs/helper.py
+++ cogs/helper.py
@@ -6,7 +6,7 @@ class DiscordHelp(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    @command()
+    @command(alias=["도움", "도움말"])
     async def help(self, ctx):
         await ctx.send(
             ctx.author.mention + """ 안녕하세요! 이 봇은 __**Discord + Web**__ 테스트 중입니다.
@@ -14,6 +14,7 @@ class DiscordHelp(commands.Cog):
 문의가 필요하시면 아래 연락처로 연락바랍니다.
 
 __**Github**__: <https://github.com/R3turn-Dev/TwitchsTalker>
+__**Web**__: http://bot.return0927.xyz/
 __**Admin**__: 이은학#9999 / invoice@return0927.xyz"""
         )
 
